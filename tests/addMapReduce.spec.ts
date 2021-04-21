c213fe15aa01b2c0c5d8d82a7340f94407f0f7f8
Eduardo Brito <alu0100783315@ull.edu.es>
2021-04-21 11:00:26 +0100

    Doc generated

--- tests/addMapReduce.spec.ts
+++ tests/addMapReduce.spec.ts
@@ -6,15 +6,16 @@ const amrInstance: AddMapReduce = new AddMapReduce([1, 2, 3], (val: number) => {
   val = val + 1;
   return val;
 });
-describe('AddMapReduce class methods tested with a AddMapReduce class instance', () => {
-  describe('Testing map', () => {
-    it('amrInstance.map should be [2, 3, 4]', () => {
-      expect(amrInstance.map()).to.eql([2, 3, 4]);
+describe('AddMapReduce class methods tested with a AddMapReduce class instance',
+  () => {
+    describe('Testing map', () => {
+      it('amrInstance.map should be [2, 3, 4]', () => {
+        expect(amrInstance.map()).to.eql([2, 3, 4]);
+      });
+    });
+    describe('Testing run', () => {
+      it('amrInstance.run should be 9', () => {
+        expect(amrInstance.run()).to.eql(9);
+      });
     });
   });
-  describe('Testing run', () => {
-    it('amrInstance.run should be 9', () => {
-      expect(amrInstance.run()).to.eql(9);
-    });
-  });
-});
