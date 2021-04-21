c1508b8b50f195e7edacca100b69fa480706b86a
Eduardo Brito <alu0100783315@ull.edu.es>
2021-04-21 10:51:09 +0100

    Div tests failing

---
+++ tests/divMapReduce.spec.ts
@@ -0,0 +1,17 @@
+import 'mocha';
+import { expect } from 'chai';
+import { DivMapReduce } from '../src/divMapReduce';
+
+const dmrInstance: DivMapReduce =
+  new DivMapReduce([4, 2, 1], (val: number) => {
+    val = val + 1;
+    return val;
+  });
+describe('DivMapReduce class methods tested with a class instance',
+  () => {
+    describe('Testing run', () => {
+      it('dmrInstance.run should be 2', () => {
+        expect(dmrInstance.run()).to.eql(2);
+      });
+    });
+  });
